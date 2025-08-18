6cc877f9f2cde5e4e3ff5b10ba4b1669d7f6872c
msohaaib <msohaib9685055@gmail.com>
2025-08-19 03:54:51 +0500

    updated

--- src/Component/FeatureSections.jsx
+++ src/Component/FeatureSections.jsx
@@ -2,6 +2,7 @@ import React, { useRef } from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
 import { FiArrowUpRight } from "react-icons/fi";
 import { Link } from "react-router-dom";
+motion;
 
 const ChannelsSection = () => {
   const channels = [
